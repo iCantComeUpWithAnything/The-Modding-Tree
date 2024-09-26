517d2b8129793c52a17d2fbc2b14e17d9faac3d9
iCantComeUpWithAnything <omegaboi141241@gmail.com>
2024-09-26 11:28:33 +0200

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Boiler Tree",
+	id: "averycomplexnamethatsreallyoriginalandneverwillbecopied",
+	author: "NotABot",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
